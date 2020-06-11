a032bbe769f6d91e7e723a9e4fbf5264296bff8c
yaroslavas2001 <61469500+yaroslavas2001@users.noreply.github.com>
2020-06-11 13:52:58 +0300

    gap

--- js/mian-es6.js
+++ js/mian-es6.js
@@ -20,10 +20,10 @@ menu.classList.add('header__nav_active');
 menuCloseitem.addEventListener('click',() => {
   menu.classList.remove('header__nav_active');
 });
-if(window.innerWidth<768){
-for(let i=0; i < menuLinks.length;i+=1){
-  menuLinks[i].addEventListener('click',()=>{
-menu.classList.remove('.header__nav_active');
+if(window.innerWidth < 768) {
+for(let i=0; i < menuLinks.length;i += 1) {
+  menuLinks[i].addEventListener('click', () => {
+menu.classList.remove('header__nav_active');
   });
 }
 }
